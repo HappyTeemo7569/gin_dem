af77cf2a9d00c7aee53eae377cb0f1bf57e4d5d7
HappyTeemo7569 <1726214027@qq.com>
2022-11-04 14:26:50 +0800

    feat: redirect

---
+++ server/redirect.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Redirect1(c *gin.Context) {
+	//重定向到到百度，获取百度对应的数据；
+	//重定向状态码：StatusMovedPermanently
+	url := "http://www.baidu.com" //重定向的URL
+	c.Redirect(http.StatusMovedPermanently, url)
+}
+
+func Redirect2(c *gin.Context) {
+	c.Request.URL.Path = "/TestRedirect" //重定向的路由
+	GinEngine.HandleContext(c)
+}
+func TestRedirect(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"msg":  "响应成功",
+	})
+}
--- server/router.go
+++ server/router.go
@@ -3,4 +3,11 @@ package server
 func InitRouter() {
 	GinEngine.GET("/get", GetMsg)    //get方法
 	GinEngine.POST("/post", PostMsg) //post方法
+
+	//一般重定向:重定向到外部网络
+	GinEngine.GET("/redirect1", Redirect1)
+	//路由重定向：重定向到具体的路由
+	GinEngine.GET("/redirect2", Redirect2)
+	//路由：127.0.0.1:9090/TestRedirect
+	GinEngine.GET("/TestRedirect", TestRedirect)
 }
